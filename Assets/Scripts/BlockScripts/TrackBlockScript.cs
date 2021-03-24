cd76c116e89a46fe43e34b0fb50c27157b1a1141
ArnavKoshy <arnav.koshy@gmail.com>
2021-03-24 19:21:10 +0000

    Worked a bit more on tracks

--- Assets/Scripts/BlockScripts/TrackBlockScript.cs
+++ Assets/Scripts/BlockScripts/TrackBlockScript.cs
@@ -9,6 +9,8 @@ public class TrackBlockScript : Block
 
     private Animator anim;
     private Rigidbody2D mrig;
+    public float forward_friction;
+    public float lateral_friction;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +21,15 @@ public class TrackBlockScript : Block
     // Update is called once per frame
     void Update()
     {
+        float mass = mrig.mass;
         Vector2 vel = mrig.GetPointVelocity(transform.position);
-        float speed = Vector2.Dot(transform.forward, vel);
-        anim.SetFloat("speed", speed);
+        
+        float speed = Vector2.Dot(transform.right, vel);
+        float across = Vector2.Dot(transform.up, vel);
+        mrig.AddForceAtPosition(- transform.right * forward_friction * mass * speed, transform.position);
+        mrig.AddForceAtPosition(- transform.up * lateral_friction * mass * across, transform.position);
+        //Debug.Log(transform.forward);
+        //Debug.Log(vel);
+        anim.SetFloat("Speed", speed * 0.25f);
     }
 }
